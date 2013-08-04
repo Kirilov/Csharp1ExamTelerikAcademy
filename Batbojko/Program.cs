a9f84560e52b99bc8645fed84a50ff4cc0c0989c
Krasimir Kirilov <kirilovsd@gmail.com>
2013-08-04 17:38:04 +0300

    Second Git

---
+++ Batbojko/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int dotCount = N-1;
+        int dotcount2 = 0;
+        int lineCount = 0;
+        int indexline =2 ;
+        int adder = 2;
+
+        for (int i = 1; i <= N; i++)
+        {
+            Console.Write(new string('.', dotCount));
+            Console.Write('/');
+            if (i==indexline)
+            {
+                Console.Write(new string('-', dotcount2));
+                indexline = indexline + adder;
+                adder++;
+            }
+            else
+            {
+                Console.Write(new string('.', lineCount));
+            }            
+            Console.Write('\\');
+            Console.Write(new string('.', dotCount));           
+            Console.WriteLine();
+                       
+            dotCount--;
+            lineCount+=2;
+            dotcount2 += 2;            
+        }
+        
+    }
+}
+
---
+++ Garden/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+        double tomBr = double.Parse(Console.ReadLine());
+        double tomArea = double.Parse(Console.ReadLine());
+        double cucBr = double.Parse(Console.ReadLine());
+        double cucArea = double.Parse(Console.ReadLine());
+        double potBr = double.Parse(Console.ReadLine());
+        double potArea = double.Parse(Console.ReadLine());
+        double carBr = double.Parse(Console.ReadLine());
+        double carArea = double.Parse(Console.ReadLine());
+        double cabBr = double.Parse(Console.ReadLine());
+        double cabArea = double.Parse(Console.ReadLine());
+        double beanBr = double.Parse(Console.ReadLine());
+        //double beanArea = 0;
+
+        // ptice
+        double tomPrice = 0.5;
+        double cucPrice = 0.4;
+        double potPrice = 0.25;
+        double carPrice = 0.6;
+        double cabPrice = 0.3;
+        double beanPrice = 0.4;
+
+        double totalCost = 0;
+        double allArea = 250;
+        double leftArea = 0;
+
+        totalCost = (tomBr * tomPrice) + (cucBr * cucPrice) + (potBr * potPrice) 
+            + (carBr * carPrice) + (cabBr * cabPrice) + (beanBr * beanPrice);
+
+        leftArea = allArea - (tomArea+cucArea+potArea+carArea+cabArea);
+        Console.WriteLine("Total costs: {0:0.00}", totalCost);
+        if (leftArea==0)
+        {
+            Console.WriteLine("No area for beans");
+        }
+        else if (leftArea<0)
+        {
+            Console.WriteLine("Insufficient area");
+        }
+        else
+        {
+            Console.WriteLine("Beans area: {0}", leftArea);
+        }        
+    }
+}
+
